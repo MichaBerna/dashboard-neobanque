ffe6ed8463b6b4e05736f5006563df7b808d1155
MichaBerna <michael.bernasinski@gmail.com>
2025-09-01 08:48:00 +0200

    fix: fix Model loader

--- api/model/model_loader.py
+++ api/model/model_loader.py
@@ -1,6 +1,10 @@
+import os
+
 import joblib
 
+this_dir = os.path.dirname(__file__)
 
-def load_model(model_path="modele/modele_prediction_credit.joblib"):
+
+def load_model(model_path=f"{this_dir}/../../modele/modele_prediction_credit.joblib"):
     model_data = joblib.load(model_path)
     return model_data["model"], model_data["seuil"], model_data["preprocessor"]
