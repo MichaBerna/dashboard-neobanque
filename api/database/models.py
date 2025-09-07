1db306b229ea5214d1fcd4d22bbbef903fce8b08
MichaBerna <michael.bernasinski@gmail.com>
2025-09-07 22:17:58 +0200

    fix : fix warning import declarative_base

--- api/database/models.py
+++ api/database/models.py
@@ -3,7 +3,7 @@ from datetime import date
 from typing import Any
 
 from sqlalchemy import Column, Float, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 from api.security.encryption import decrypt, encrypt
 
