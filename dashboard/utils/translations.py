a835a6e98dff37bf4afd72e099ca708c73b3aa91
MichaBerna <michael.bernasinski@gmail.com>
2025-10-14 14:47:31 +0200

    fix: Correction loading translations

--- dashboard/utils/translations.py
+++ dashboard/utils/translations.py
@@ -3,6 +3,7 @@ import os
 
 
 def load_translations(lang):
-    file_path = os.path.join("locales", f"{lang}.json")
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    file_path = os.path.join(project_root, "locales", f"{lang}.json")
     with open(file_path, encoding="utf-8") as tradfile:
         return json.load(tradfile)
